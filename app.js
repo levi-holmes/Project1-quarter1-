eeabcce41bb5d197bf24d584fa92a4d6484b9f3d
Levi Holmes <levi.holmes81@gmail.com>
2017-06-28 16:14:36 -0600

    3  user stories completed.

--- app.js
+++ app.js
@@ -3,17 +3,27 @@ var $SetSelect = $('#cardSets')
 $.get('https://api.magicthegathering.io/v1/sets/')
   .then(function(data){
     console.log(data);
-for (var i = 0; i < data.sets.length; i++) {
+for (var i = 1; i < data.sets.length; i++) {
   $SetSelect.append("<option><p>" + data.sets[i].name + "</p></option>")
 }
 })
 
-var $select = $('#cardSets')
+var $Costselect = $('#cardCost')
 
-$.get('https://api.magicthegathering.io/v1/sets/')
+$.get('https://api.magicthegathering.io/v1/cards/')
   .then(function(data){
     console.log(data);
-for (var i = 0; i < data.sets.length; i++) {
-  $select.append("<option><p>" + data.sets[i].name + "</p></option>")
+for (var i = 0; i < 17; i++) {
+  $Costselect.append("<option><p>" + i + "</p></option>")
 }
 })
+
+// var $Rarityselect = $('#')
+//
+// $.get('https://api.magicthegathering.io/v1/cards/')
+//   .then(function(data){
+//     console.log(data);
+// for (var i = 0; i < data.sets.length; i++) {
+//   $Rarityselect.append("<option><p>" + data.sets[i].rarity + "</p></option>")
+// }
+// })
