111dadc45742c762111cb818adf00e59a6ff005e
Levi Holmes <levi.holmes81@gmail.com>
2017-06-29 14:56:13 -0600

    removed old redundant data for manually populating pull down menus

--- app.js
+++ app.js
@@ -17,15 +17,3 @@ for (var i = 0; i < 17; i++) {
   $Costselect.append("<option><p>" + i + "</p></option>")
 }
 })
-
-// var $Rarityselect = $('#')
-//
-// $.get('https://api.magicthegathering.io/v1/cards/')
-//   .then(function(data){
-//     console.log(data);
-// for (var i = 0; i < data.sets.length; i++) {
-//   $Rarityselect.append("<option><p>" + data.sets[i].rarity + "</p></option>")
-// }
-// })
-
-console.log(CBLOCK, CSETS, RARITY, COLOR, CMC, CTYPE);
