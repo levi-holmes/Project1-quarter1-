a693fcc30d0993ebf9a684eefc90ec2886fb15ea
Levi Holmes <levi.holmes81@gmail.com>
2017-06-29 23:59:50 -0600

    calling it a night. hopefully I can get this running by 1030am.

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+var apiURLcards = "https://api.magicthegathering.io/v1/cards/?"
+
+var apiURLsets = "https://api.magicthegathering.io/v1/sets/?"
+
+
 var $SetSelect = $('#cardSets')
 
-$.get('https://api.magicthegathering.io/v1/sets/')
+$.get(apiURLsets)
   .then(function(data){
     console.log(data);
 for (var i = 1; i < data.sets.length; i++) {
@@ -10,10 +15,52 @@ for (var i = 1; i < data.sets.length; i++) {
 
 var $Costselect = $('#cardCost')
 
-$.get('https://api.magicthegathering.io/v1/cards/')
+$.get(apiURLcards)
   .then(function(data){
     console.log(data);
 for (var i = 0; i < 17; i++) {
   $Costselect.append("<option><p>" + i + "</p></option>")
 }
 })
+
+
+// var apiURL = "https://api.magicthegathering.io/v1/cards?"
+//
+// var getUrl = apiURL + colorSelect + typeSelect + raritySelect + eternalSelect + modernSelect + nameSelect;
+//
+// document.getElementById('searchButton').click(function() {
+//   alert('I was clicked')
+// })
+//https://api.magicthegathering.io/v1/cards?name=elf
+
+var searchName = apiURLcards + "name=" + searchInput;
+console.log(searchName);
+
+$("#searchButton").click(function(event){
+  event.preventDefault();
+  console.log(apiURLcards);
+//var searchInput = $('#search').val();
+ $.get(searchName)
+.then(function(data){
+console.log(searchInput);
+
+	});
+})
+$(".cardsContainer").empty();
+$.get(searchName.then(function(data) {
+    var cards = data.cards;
+    for (var i = 0; i < cards.length; i++) {
+      updatePage(cards[i]);
+
+    }
+})
+
+function updatePage(cards) {
+    $(".cardsContainer").append('<img src="'+ cards.imageUrl + '">')
+};
+
+
+$('.searchButton').click (function(){
+var searchInput = $('#search').text();
+apiURL + "name=" + searchInput;})
+// window.open(url);)
