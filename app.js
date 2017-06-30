75695c3ee085ff815d8fcdc786640eafb8a34526
Levi Holmes <levi.holmes81@gmail.com>
2017-06-30 14:15:57 -0600

    screenshot added

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+$("document").ready(function() {
+  $("button").click(function() {
+  })
+});
+
 var apiURLcards = "https://api.magicthegathering.io/v1/cards/?"
 
 var apiURLsets = "https://api.magicthegathering.io/v1/sets/?"
 
+var getUrl = PopulateCards + searchInput;
+
+var PopulateCards = apiURLcards + "name=";
+
+var searchInput = $('#search').val();
 
 var $SetSelect = $('#cardSets')
-
 $.get(apiURLsets)
   .then(function(data){
     console.log(data);
@@ -14,7 +23,6 @@ for (var i = 1; i < data.sets.length; i++) {
 })
 
 var $Costselect = $('#cardCost')
-
 $.get(apiURLcards)
   .then(function(data){
     console.log(data);
@@ -23,44 +31,45 @@ for (var i = 0; i < 17; i++) {
 }
 })
 
-
-// var apiURL = "https://api.magicthegathering.io/v1/cards?"
-//
-// var getUrl = apiURL + colorSelect + typeSelect + raritySelect + eternalSelect + modernSelect + nameSelect;
-//
-// document.getElementById('searchButton').click(function() {
-//   alert('I was clicked')
-// })
-//https://api.magicthegathering.io/v1/cards?name=elf
-
-var searchName = apiURLcards + "name=" + searchInput;
-console.log(searchName);
-
 $("#searchButton").click(function(event){
   event.preventDefault();
   console.log(apiURLcards);
-//var searchInput = $('#search').val();
- $.get(searchName)
-.then(function(data){
-console.log(searchInput);
+  var searchName = apiURLcards + "name=" + $('#search').val();
+  console.log(searchName);
+  $.get(searchName)
+    .then(function(data){
+      console.log(data);
+      var cards = data.cards;
+
+      for (var i = 0; i < cards.length; i++) {
+        var card = cards[i];
+        var image = card.imageUrl;
+
+        $('.cardsContainer').append('<img src="' + image + '">')
+
+      }
+
 
 	});
 })
 $(".cardsContainer").empty();
-$.get(searchName.then(function(data) {
-    var cards = data.cards;
+$.get(PopulateCards).then(function(data) {
+    var cards = data.cards.imageUrl;
     for (var i = 0; i < cards.length; i++) {
       updatePage(cards[i]);
 
     }
-})
-
-function updatePage(cards) {
-    $(".cardsContainer").append('<img src="'+ cards.imageUrl + '">')
-};
+  });
 
 
-$('.searchButton').click (function(){
-var searchInput = $('#search').text();
-apiURL + "name=" + searchInput;})
-// window.open(url);)
+
+function cardPopulator(){ $('.searchButton').click (function(){
+  });
+}
+
+  $.get(getUrl).then(function(data) {
+    var cards = data.cards;
+    for (var i = 0; i < cards.length; i++) {
+      updatePage(cards[i]);
+    }
+  })
